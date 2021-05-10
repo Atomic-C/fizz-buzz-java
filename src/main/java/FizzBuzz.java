a3d71bc9b75b57de1a4346ef1bfe5892eed42427
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-05-10 23:42:23 +0000

    initial commit includes some skeleton classes

---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,8 @@
+public class FizzBuzz {
+
+        public String convert(int convertToFizzBuzz) {
+
+            return null;
+        }
+
+}
---
+++ src/main/java/SandBox.java
@@ -0,0 +1,16 @@
+public class SandBox {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
+
+        /*
+        Write a program that prints numbers from 0 to 100;
+        On multiples of three print "Fizz" instead of the number
+        On multiples of five print "Buzz" instead of the number
+        For numbers which are multiples fo both three and five print "FizzBuzz"
+        */
