da0cfcbcbc3e919076e8300712347c41d28dde9f
pedro sousa <pedro.p.sousa@sapo.pt>
2021-05-11 23:36:25 +0000

    Pedro adds an if statement to evaluate if convertToFizzBuzz is multiple of 5, test multiplesOfFive passes

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -2,8 +2,11 @@ public class FizzBuzz {
 
         public String convert(int convertToFizzBuzz) {
 
-            if(convertToFizzBuzz%3==0) {
+            if(convertToFizzBuzz %3 == 0) {
                 return "Fizz";
+            }
+            else if(convertToFizzBuzz % 5 ==0 ) {
+                return "Buzz";
             }
 
             return String.valueOf(convertToFizzBuzz);
