872af289bdb50e8a0f07e434182587440611d06a
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-05-14 22:48:38 +0000

    Pedro leaves TODO to figure out how to fix tests.

--- src/main/java/CountToOneHundred.java
+++ src/main/java/CountToOneHundred.java
@@ -5,12 +5,14 @@ public class CountToOneHundred {
 
     private List<Integer> myList = new ArrayList<Integer>();
 
+
     public List<Integer> outputTheHundredFizzBuzzes() {
 
 
         for (int i = 1; i <= 100 ; i++) {
 
-              myList.add(i);
+
+            myList.add(i); // We successfully add 100 integers to our list!
             //System.out.println(i);
 
         }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -2,6 +2,8 @@ import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -9,13 +11,24 @@ public class FizzBuzzTest {
 
     FizzBuzz fizzBuzz = new FizzBuzz();
 
+    private List<Integer> myList = new ArrayList<Integer>();
+
+    private List<Integer> myListForTest = new ArrayList(100);
+
+    // sources I'm working with ATM: https://docs.oracle.com/javase/7/docs/api/java/util/List.html
+    // ALSO: https://www.baeldung.com/java-list-capacity-array-size
+    //TODO: FIGURE OUT HOW TO ADD TO LIST, ACCESS AND PASS TO CONVERTER. Currently asking for: List type
+
+
     @Test
     public void convertLeavesNormalNumbersAlone() {
+        myList.add(1);
+        myList.add(2);
 
         String inputTest = "3";
         InputStream in = new ByteArrayInputStream(inputTest.getBytes());
         System.setIn(in);
-        assertEquals("1", fizzBuzz.convert(1));
+        assertEquals("1", fizzBuzz.convert(myList.subList(0,1)));
         assertEquals("2", fizzBuzz.convert(2));
         assertEquals("4", fizzBuzz.convert(4));
 
